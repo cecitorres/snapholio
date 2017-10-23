95d083fd852074993ce34d6cbbc971e9cf7588ad
Cecilia <cecilia.torres@iiiepe.edu.mx>
2017-10-23 17:46:54 -0500

    Config database y cambio en Title

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://CHANGEME'}
+  module.exports = {mongoURI: 'mongodb://root:123@ds231205.mlab.com:31205/snapholio-dev'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/snapholio-dev'}
 }
