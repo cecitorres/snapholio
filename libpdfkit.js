562272a7bc09b489fe3df51e3221cf28ce5db3ba
Ceci Torres <cecilia.torres@iiiepe.edu.mx>
2017-11-07 11:42:31 -0600

    pdfkit

--- libpdfkit.js
+++ libpdfkit.js
@@ -1,27 +1,38 @@
 const fs = require('fs')
 const PDFDocument = require('pdfkit');
 
-let pdf = new PDFDocument({
-  size: 'LETTER',
-  info: {
-    Title: 'Tile of File Here',
-    Author: 'Some Author',
-  }
-});
+const libpdf = {};
 
-// Write stuff into PDF
-pdf.text('Hello World');
+libpdf.generatePortfolio = (user, imgs) => {
 
-doc.image('images/test.jpeg', 0, 15, {width: 300})
-.text('Proportional to width', 0, 0)
-
-// Stream contents to a file
-pdf.pipe(
-  fs.createWriteStream('./path/to/file.pdf')
-)
-  .on('finish', function () {
-    console.log('PDF closed');
+  let pdf = new PDFDocument({
+    size: 'LETTER',
+    info: {
+      Title: 'Portfolio by ' + user.name,
+      Author: user.email,
+    }
   });
 
-// Close PDF and write file.
-pdf.end();
+  // Write stuff into PDF
+  pdf.text('Hello World');
+  imgs.forEach(img => {
+    pdf.image(img.image.imageUrl, 0, 15, {
+        width: 300
+      })
+      .text(img.title, 0, 0);
+  });
+
+  // Stream contents to a file
+  pdf.pipe(
+      fs.createWriteStream('./pruebafile.pdf')
+    )
+    .on('finish', function () {
+      console.log('PDF closed');
+    });
+
+  // Close PDF and write file.
+  pdf.end();
+
+}
+
+module.exports = libpdf;
