3d063ecef6264712681f611d10ab3aae91f39e1e
Ceci Torres <cecilia.torres@iiiepe.edu.mx>
2017-11-13 02:14:53 -0600

    Formateo al pdf en columnas

--- repositories/Portfolio.js
+++ repositories/Portfolio.js
@@ -6,27 +6,41 @@ const Portfolio = {};
 
 Portfolio.generate = (user, imgs) => {
   let pdf = new PDFDocument({
-    size: 'LETTER',
+    size: 'LEGAL',
     info: {
       Title: 'Portfolio by ' + user,
       Author: user,
     }
   });
 
-  pdf.fontSize(32);
+  // pdf.image(path.join(__dirname, '../', 'public/img/logo256x256.png'), (pdf.page.width-50)/2, 20,{
+  //   height: 50,
+  //   align: 'center'
+  // });
+  pdf.fontSize(18);
   // Write stuff into PDF
-  pdf.text('Portfolio by ' + user, {
+  pdf.text('Portfolio by ' + user, 70, 30, {
     align: 'center'
   });
-  pdf.fontSize(18);
-  imgs.forEach(img => {
-    pdf.text(img.title)
-      .image(path.join(__dirname, '../', 'public/') + img.image.imageUrl, {
-        width: 300
-      })
-      .text('-------------------------------------------', {
-        align: 'center'
+  pdf.fontSize(14);
+  imgs.forEach((img, index) => {
+    let xImg = index % 2 == 0 ? 70 : 350;
+    let xTxT = index % 2 == 0 ? -200 : 350;
+    let yImg;
+
+    if(index < 2) yImg = 60;
+    else if(index < 4) yImg = 290;
+    else yImg = 520;
+
+    pdf.text(img.title, xTxT, yImg + 210, {align: 'center'})
+      .image(path.join(__dirname, '../', `public/${img.image.imageUrl}`), xImg, yImg, {
+      height: 200
       });
+      
+  });
+
+  pdf.text('This is a footer', 20, pdf.page.height - 50, {
+    lineBreak: false
   });
 
   // Stream contents to a file
